238320fa65e7cffd78b739531f5f525537d1fcd1
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2022-03-21 16:21:26 +0100

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -20,6 +20,7 @@ st.write('The weather of', location)
 url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey
 response = requests.get(url)
 weatherData = json.loads(response.text)
+st.write(weatherData)
 
 
 
