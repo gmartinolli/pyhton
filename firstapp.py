04ca9affa7c5a7191e70a3eb300ea554acad746b
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2022-03-21 16:20:22 +0100

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -16,7 +16,7 @@ import json, requests
 APIkey = "e8d8bf91c4a17c3cd68e16fc49886d0b"
 import streamlit as st
 location = st.text_input('Input city name', 'city name')
-st.write('The weather of', title)
+st.write('The weather of', location)
 url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey
 response = requests.get(url)
 weatherData = json.loads(response.text)
