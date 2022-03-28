0f8bfad69b0c7dcea759b445489ee16162ca5246
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2022-03-28 16:45:34 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -3,8 +3,7 @@ st.header("Word search")
 st.text("please select one")  
 import json,requests
 from pprint import pprint
-keyword= st.text_input("choose a word")
-
+keyword = st.text_input("choose a word")
 search = st.selectbox("now choose what you are most interested in",('sounds like', 'looks like', 'is opposite to', 'is similar to'))
 st.write('your word')
 url1= 'https://api.datamuse.com/words?sl=' + keyword + '&max=10'
@@ -19,13 +18,13 @@ dataFromDatamuse3 = json.loads(response3.text)
 url4= 'https://api.datamuse.com/words?syn=' + keyword + '&max=10'
 response4 = requests.get(url4)  
 dataFromDatamuse4 = json.loads(response4.text)
-if keyword == 'sounds like':
+if search == 'sounds like':
      st.write('Your word sounds like',dataFromDatamuse1)
-if keyword == 'looks like':
+if search == 'looks like':
      st.write('Your word looks like',dataFromDatamuse2)
-if keyword == 'is opposite to':
+if search == 'is opposite to':
      st.write('Your word is the opposite of',dataFromDatamuse3)
-if keyword == 'is similar to':
+if search == 'is similar to':
      st.write('Your word is similar to',dataFromDatamuse4)
 
 
