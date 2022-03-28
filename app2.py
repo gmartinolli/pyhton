be00554f98295224508434fd17f00c7573b978d6
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2022-03-28 16:27:24 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -3,9 +3,16 @@ st.header("Word search")
 st.text("please select one")  
 import json,requests
 from pprint import pprint
-keyword=input('plz give me a keyword')
-import streamlit as st
-keyword = st.radio("Words that...",('sound like', 'look like', 'are opposite to', 'are similar to'))
-st.write('your word')
+st.text_input("choose a word")
 
+keyword = st.radio("Words that...",('sounds like', 'looks like', 'is opposite to', 'is similar to'))
+st.write('your word')
+if keyword == 'sounds like':
+     st.write('Your word sounds like')
+if keyword == 'looks like':
+     st.write('Your word looks like')
+if keyword == 'is opposite to':
+     st.write('Your word is the opposite of')
+if keyword == 'is similar to':
+     st.write('Your word is similar to')
 url1= 'https://api.datamuse.com/words?syn=' + keyword + '&max=4'
