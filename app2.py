3cf4586e016618caa8b135b0a68cef477b0e07c5
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2022-03-28 16:20:25 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -6,6 +6,6 @@ from pprint import pprint
 keyword=input('plz give me a keyword')
 import streamlit as st
 keyword = st.radio("Words that...",('sound like', 'look like', 'are opposite to', 'are similar to'))
-st.write('')
+st.write('your word')
 
 url1= 'https://api.datamuse.com/words?syn=' + keyword + '&max=4'
