c660aa267c6411c43e1d14e99f6105685e53c90e
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2022-04-04 16:29:34 +0200

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,1 +1,9 @@
-
+from googletrans import Translator
+translator = Translator()
+st.header("Google translator")  
+keyword1 = st.text_input("translate")  
+keyword2 = st.text_input("into which language?")
+word= keyword1 
+abc = translator.translate(word,dest= keyword2)
+st.write('the given word is in:', abc.src)
+st.write('in', keyword2, 'your word means:', abc.text)
