cfed8a101cab4ca15a8ac244ad889de4bfcaf742
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2022-04-04 16:33:34 +0200

    Update app3.py

--- app3.py
+++ app3.py
@@ -3,7 +3,7 @@ from googletrans import Translator
 translator = Translator()
 st.header("Google translator")  
 keyword1 = st.text_input("translate")  
-keyword2 = st.text_input("into which language?")
+keyword2 = st.text_input("into which language?", value="en")
 word= keyword1 
 abc = translator.translate(word,dest= keyword2)
 st.write('the given word is in:', abc.src)
