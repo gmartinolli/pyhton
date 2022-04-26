9a72f761df4d0c63a842a9a4a3b8cf879a41de15
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2022-04-26 13:15:35 +0200

    Update texttospeech.py

--- texttospeech.py
+++ texttospeech.py
@@ -4,8 +4,8 @@ import IPython.display as ipd
 from googletrans import Translator
 translator = Translator()
 
-text = st.text_input("type a text")
-lang = st.text_input("choose a language")
+text = st.text_input("type a text",'some text')
+lang = st.text_input("choose a language", 'en')
 detectedLanguage = translator.detect(text)
 st.write(detectedLanguage.lang)
 transTodest = translator.translate(text=text, src=detectedLanguage.lang, dest=lang)
