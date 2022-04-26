4cd2d7b77bb3497d41fa208220b36d75b5485140
gmartinolli <102040145+gmartinolli@users.noreply.github.com>
2022-04-26 13:09:40 +0200

    Update and rename texttospeech to texttospeech.py

---
+++ texttospeech.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from gtts import gTTS
+import IPython.display as ipd  
+from googletrans import Translator
+translator = Translator()
+
+text = st.text_input("type a text")
+lang = st.text_input("choose a language")
+detectedLanguage = translator.detect(text)
+st.write(detectedLanguage.lang)
+transTodest = translator.translate(text=text, src=detectedLanguage.lang, dest=lang)
+print(transTodest.text)
+
+#tts1=gTTS(transTodest.text,lang)
+#tts1.save('audio.mp3')
+#st.audio(audio.mp3)
+#my_function('audio.mp3')
+
